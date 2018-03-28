b8cd161c120a7b4ae2357e97f08d2a0bb8cf9fc7
10395803 <subbiali@phys.ethz.ch>
2018-03-28 09:52:00 +0200

    Fix isentropic dycore by actually diagnosing temperature at each time step.

--- scripts/script_contourf_animation_xz.py
+++ scripts/script_contourf_animation_xz.py
@@ -11,29 +11,29 @@ import tasmania.utils.utils_plot as utils_plot
 # Mandatory settings
 #
 filename    = os.path.join(os.environ['TASMANIA_ROOT'], 'data/verification_kessler_wrf_maccormack_animation.pickle')
-field       = 'x_velocity'
+field       = 'mass_fraction_of_cloud_liquid_water_in_air'
 y_level     = 0
-destination = os.path.join(os.environ['TASMANIA_ROOT'], '../meetings/20180328_status_meeting/movie/verification_kessler_wrf_maccormack_x_velocity.mp4')
+destination = os.path.join(os.environ['TASMANIA_ROOT'], '../meetings/20180328_status_meeting/movie/verification_kessler_wrf_maccormack_x_cloud_liquid_water.mp4')
 
 #
 # Optional settings
 #
 fontsize         = 16
 figsize          = [7,8]
-title            = '$x$-velocity [m s$^{-1}$]'
+title            = 'Cloud liquid water [g kg$^{-1}$]'
 x_factor         = 1.e-3
 x_label          = '$x$ [km]'
 x_lim			 = None #[-40,40]
 z_factor         = 1.e-3
 z_label			 = '$z$ [km]'
 z_lim            = [0,10]
-field_factor     = 1.
+field_factor     = 1.e3
 plot_height		 = True
-cmap_name        = 'BuRd' # Alternatives: Blues, BuRd, jet, RdBu, RdYlBu, RdYlGn
+cmap_name        = 'Blues' # Alternatives: Blues, BuRd, jet, RdBu, RdYlBu, RdYlGn
 cbar_levels      = 18
 cbar_ticks_step  = 4
-cbar_center      = 15.
-cbar_half_width  = 9.5
+cbar_center      = None #15.
+cbar_half_width  = None #9.5
 cbar_x_label     = None
 cbar_y_label     = None
 cbar_title       = None
