9e86b8f7810238f385625b02d5ab64dd337730ab
10395803 <subbiali@phys.ethz.ch>
2018-03-28 08:40:29 +0200

    Add test for Kessler.

--- tests/test_adjustment_kessler.py
+++ tests/test_adjustment_kessler.py
@@ -26,6 +26,7 @@ Script to test the class AdjustmentMicrophysicsKessler.
 from datetime import datetime, timedelta
 import numpy as np
 
+import gridtools as gt
 from tasmania.grids.grid_xyz import GridXYZ
 from tasmania.parameterizations.adjustment_microphysics import AdjustmentMicrophysics
 from tasmania.storages.state_isentropic import StateIsentropic
@@ -37,13 +38,13 @@ grid = GridXYZ(domain_x, nx, domain_y, ny, domain_z, nz)
 
 dt = timedelta(seconds = 5.4)
 
-rho = np.random.rand((nx, ny, nz), dtype = float)
-p   = np.random.rand((nx, ny, nz+1), dtype = float)
-exn = np.random.rand((nx, ny, nz+1), dtype = float)
-T   = np.random.rand((nx, ny, nz), dtype = float)
-qv  = np.random.rand((nx, ny, nz), dtype = float)
-qc  = np.random.rand((nx, ny, nz), dtype = float)
-qr  = np.random.rand((nx, ny, nz), dtype = float)
+rho = np.random.rand(nx, ny, nz)
+p   = np.random.rand(nx, ny, nz+1)
+exn = np.random.rand(nx, ny, nz+1)
+T   = 273. + np.random.rand(nx, ny, nz)
+qv  = np.random.rand(nx, ny, nz)
+qc  = np.random.rand(nx, ny, nz)
+qr  = np.random.rand(nx, ny, nz)
 state = StateIsentropic(datetime(year = 1992, month = 2, day = 20), grid,
 						air_density                                 = rho,
 						air_pressure                                = p,
@@ -51,9 +52,10 @@ state = StateIsentropic(datetime(year = 1992, month = 2, day = 20), grid,
 						air_temperature                             = T,
 						mass_fraction_of_water_vapor_in_air         = qv,
 						mass_fraction_of_cloud_liquid_water_in_air  = qc,
-						mass_fraction_of_orecipitation_water_in_air = qr)
+						mass_fraction_of_precipitation_water_in_air = qr)
 
-mp = AdjustmentMicrophysics('kessler', grid, False, gt.mode.NUMPY)
+mp = AdjustmentMicrophysics.factory('kessler', grid, False, gt.mode.NUMPY)
+mp.time_levels = 1
 
 state_new, diagnostics = mp(dt, state)
 
