f43a3ff9baa47bcc8d94de26421c29fee6836e69
10395803 <subbiali@phys.ethz.ch>
2018-04-25 16:54:15 +0200

    Minor changes.

--- scripts/script_animation_profile_x_comparison.py
+++ scripts/script_animation_profile_x_comparison.py
@@ -16,101 +16,102 @@ import tasmania.utils.utils_plot as utils_plot
 #
 # Mandatory settings
 #
-filename1    = os.path.join(os.environ['TASMANIA_ROOT'], 
-							'data/verification_kessler_wrf_sedimentation_leapfrog.pickle')
-field1       = 'accumulated_precipitation'
-filename2    = os.path.join(os.environ['TASMANIA_ROOT'], 
-							'data/verification_kessler_wrf_sedimentation_evaporation_leapfrog.pickle')
-field2       = 'accumulated_precipitation'
-y_level     = 0
-z_level     = -1
+filename    = [
+			   os.path.join(os.environ['TASMANIA_ROOT'], 
+			   				'data/verification_kessler_wrf_second_order_sedimentation_evaporation_maccormack.pickle'),
+			   os.path.join(os.environ['TASMANIA_ROOT'], 
+			   				'data/slow_tendency_kessler_wrf_sedimentation_evaporation_maccormack.pickle'),
+			   #os.path.join(os.environ['TASMANIA_ROOT'], 
+			   #			'data/verification_kessler_wrf_second_order_sedimentation_maccormack.pickle'),
+			   #os.path.join(os.environ['TASMANIA_ROOT'], 
+			   #			'data/verification_kessler_wrf_second_order_sedimentation_evaporation_maccormack.pickle'),
+			   ]
+field       = [
+			   'precipitation',
+			   'precipitation',
+			   'precipitation',
+			   'precipitation',
+			  ]
+y_level     = [0, 0, 0, 0]
+z_level     = [-1, -1, -1, -1]
 destination = os.path.join(os.environ['TASMANIA_ROOT'], 
-						   'results/movies/kessler_sedimentation_evaporation/verification_kessler_wrf_sedimentation_evaporation_leapfrog_accumulated_precipitation.mp4')
+						   'results/movies/kessler_sedimentation_evaporation/kessler_sedimentation_evaporation_slow_tendency_adjustment_precipitation.mp4')
 
 #
 # Animation settings
 #
 fontsize         = 16
 figsize          = [7,8]
-title            = 'Accumulated precipitation [mm]'
+title            = 'Precipitation [mm h$^{-1}$]'
 x_factor         = 1.e-3
 x_label          = '$x$ [km]'
 x_lim			 = None #[-40,40]
-y_factor1        = 1. #1.e-3
-y_factor2        = 1. #1.e-3
 y_label			 = '' #'$z$ [km]'
-y_lim            = [0,1.2]
-color1           = 'green'
-linestyle1		 = '-'
-linewidth1       = 1.5
-color2           = 'green'
-linestyle2		 = '--' 
-linewidth2       = 1.5
+y_lim            = [0,0.7]
+field_factor     = [1., 1., 1., 1.]
+color            = ['blue', 'orange', 'blue', 'blue']
+linestyle		 = ['-', '--', '-', '-.']
+linewidth        = [1.5, 1.5, 1.5, 1.5]
 grid_on			 = True
-fps				 = 7
-legend1			 = 'Rain evap. OFF'
-legend2			 = 'Rain evap. ON'
+fps				 = 6
+legend			 = ['MC ADJ', 'MC ST', 'MC, evap. OFF', 'MC, evap. ON']
 legend_location  = 'upper right'
 
 #
-# Load first field
+# Load the fields
 #
-with open(filename1, 'rb') as data:
-	state_save = pickle.load(data)
-	diagnostics_save = pickle.load(data)
+for m in range(len(filename)):
+	with open(filename[m], 'rb') as data:
+		state_save = pickle.load(data)
+		diagnostics_save = pickle.load(data)
 
-	if False: # The field to plot is a state variable
-		time = state_save[field1].coords['time'].values 
-		var1 = state_save[field1].values[:, y_level, z_level, :]
-		x1 = state_save.grid.x.values if var1.shape[0] == state_save.grid.nx \
-			 else state_save.grid.x_half_levels.values
-
-	if True:  # The field to plot is a diagnostic
-		time = diagnostics_save[field1].coords['time'].values 
-		var1 = diagnostics_save[field1].values[:, y_level, z_level, :]
-		x1 = diagnostics_save.grid.x.values if var1.shape[0] == diagnostics_save.grid.nx \
-			 else diagnostics_save.grid.x_half_levels.values
-
-#
-# Load second field
-#
-with open(filename2, 'rb') as data:
-	state_save = pickle.load(data)
-	diagnostics_save = pickle.load(data)
-
-	if False: # The field to plot is a state variable
-		var2 = state_save[field2].values[:, y_level, z_level, :]
-		x2 = state_save.grid.x.values if var2.shape[0] == state_save.grid.nx \
-			 else state_save.grid.x_half_levels.values
-
-	if True:  # The field to plot is a diagnostic
-		var2 = diagnostics_save[field2].values[:, y_level, z_level, :]
-		x2 = diagnostics_save.grid.x.values if var2.shape[0] == diagnostics_save.grid.nx \
-			 else diagnostics_save.grid.x_half_levels.values
+		if state_save[field[m]] is not None: # The field to plot is a state variable
+			if m == 0:
+				time = state_save[field[m]].coords['time'].values 
+				var_ = state_save[field[m]].values[:, y_level[m], z_level[m], :]
+				var  = var_[np.newaxis, :, :]
+				x_   = state_save.grid.x.values if var_.shape[0] == state_save.grid.nx \
+				 	   else state_save.grid.x_half_levels.values
+				x    = x_[np.newaxis, :]
+			else:
+				var_ = state_save[field[m]].values[:, y_level[m], z_level[m], :]
+				var  = np.concatenate((var, var_[np.newaxis, :, :]), axis = 0)
+				x_   = state_save.grid.x.values if var_.shape[0] == state_save.grid.nx \
+				 	   else state_save.grid.x_half_levels.values
+				x    = np.concatenate((x, x_[np.newaxis, :]), axis = 0)
+		else:  # The field to plot is a diagnostic
+			if m == 0:
+				time = diagnostics_save[field[m]].coords['time'].values 
+				var_ = diagnostics_save[field[m]].values[:, y_level[m], z_level[m], :]
+				var  = var_[np.newaxis, :, :]
+				x_   = diagnostics_save.grid.x.values if var_.shape[0] == diagnostics_save.grid.nx \
+				 	   else diagnostics_save.grid.x_half_levels.values
+				x    = x_[np.newaxis, :]
+			else:
+				var_ = diagnostics_save[field[m]].values[:, y_level[m], z_level[m], :]
+				var  = np.concatenate((var, var_[np.newaxis, :, :]), axis = 0)
+				x_   = diagnostics_save.grid.x.values if var_.shape[0] == diagnostics_save.grid.nx \
+				 	   else diagnostics_save.grid.x_half_levels.values
+				x    = np.concatenate((x, x_[np.newaxis, :]), axis = 0)
 
 #
 # Generate animation
 #
-utils_plot.animation_profile_x_comparison(time, x1, var1, x2, var2, destination,
+utils_plot.animation_profile_x_comparison(time, x, var, destination,
 										  fontsize   		= fontsize,
 										  figsize    		= figsize,
 										  title      		= title,
 										  x_factor   		= x_factor,
 										  x_label    		= x_label,
 										  x_lim		 		= x_lim,
-										  y_factor1  		= y_factor1,
-										  y_factor2  		= y_factor2,
 										  y_label	 		= y_label,
 										  y_lim      		= y_lim,
-										  color1     		= color1,
-										  linestyle1 		= linestyle1, 
-										  linewidth1 		= linewidth1,
-										  color2     		= color2,
-										  linestyle2 		= linestyle2, 
-										  linewidth2 		= linewidth2,
+										  field_factor		= field_factor,
+										  color     		= color,
+										  linestyle 		= linestyle, 
+										  linewidth 		= linewidth,
 										  grid_on    		= grid_on,
 										  fps		 		= fps,
-										  legend1	 		= legend1,
-										  legend2	 		= legend2,
+										  legend	 		= legend,
 										  legend_location 	= legend_location,
 										 )
