de2325bcd645e8ef04e9fe741a678c498d750e8f
Stefano Ubbiali <subbiali@phys.ethz.ch>
2018-08-22 12:43:51 +0200

    Merge branch 'dockerize' into new_interface

---
+++ spike/old_tests/test_gal_chen_2d.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#
+# Tasmania
+#
+# Copyright (c) 2018-2019, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the Tasmania project. Tasmania is free software:
+# you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or any later version. 
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+"""
+Test GalChen2d class. Data are retrieved from COSMO documentation, Part I.
+"""
+import math
+import numpy as np
+
+from utils import set_namelist; set_namelist()
+from grids.gal_chen import GalChen2d as Grid
+
+# Define zonal and vertical domain
+domain_x, nx = [0., 100.], 101
+domain_z, nz = [15000., 0.] , 20
+
+# Specify the interfacial height separating the terrain-following part of
+# the domain from the z-system
+zf = 11360. 
+
+# Define terrain-surface height
+topo_str = '3000. * exp(- (x - 50.)*(x - 50.) / (20.*20.) )'
+
+# Instantiate a grid object
+g = Grid(domain_x, nx, domain_z, nz, #interface_z = zf, 
+		 #topo_type = 'gaussian', topo_max_height = 3000., topo_width_x = 20)
+		topo_type = 'user_defined', topo_str = topo_str)
+
+# Plot
+g.plot()
+
+print('Test passed!')
---
+++ spike/old_tests/test_grid_xyz.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Tasmania
+#
+# Copyright (c) 2018-2019, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the Tasmania project. Tasmania is free software:
+# you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation,
+# either version 3 of the License, or any later version. 
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+"""
+Test GridXYZ.
+"""
+from grids.grid_xyz import GridXYZ
+
+domain_x, nx = [-5., 5,], 101
+domain_y, ny = [-5., 5.], 101
+domain_z, nz = [300., 300. + 50.], 20
+
+topo_str = '1. / (x*x + 1.)'
+
+xyz_grid = GridXYZ(domain_x, nx, domain_y, ny, domain_z, nz,
+				   #topo_type = 'schaer', topo_width_x = 1., topo_width_y = 2.)
+				   topo_type = 'user_defined', topo_str = topo_str)
+
+xyz_grid._topography.plot(xyz_grid.xy_grid)
+
+print('Test passed!')
