8b52699375b22506a36b7211ce02026d9d636ba2
Stefano Ubbiali <subbiali@phys.ethz.ch>
2019-01-18 11:49:20 +0100

    Updated configuration files.

--- setup.py
+++ setup.py
@@ -27,57 +27,63 @@ from setuptools import setup, find_packages
 
 
 if sys.version_info.major < 3:
-	print('Python 3.x is required.')
-	sys.exit(1)
+    print('Python 3.x is required.')
+    sys.exit(1)
 
 
 def read_file(fname):
-	"""
-	Read file into string.
+    """
+    Read file into string.
 
-	Parameters
-	----------
-	fname : str
-		Full path to the file.
+    Parameters
+    ----------
+    fname : str
+        Full path to the file.
 
-	Return
-	------
-	str :
-		File content as a string.
-	"""
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    Return
+    ------
+    str :
+        File content as a string.
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname), encoding='utf8').read()
 
 
 setup(
-	name='tasmania',
-	version='0.2.0',
-	author='Stefano Ubbiali',
-	author_email='subbiali@phys.ethz.ch',
-	description='A Python library to ease the composition, configuration, ' 
-			    'and execution of Earth system models.',
-	long_description=read_file('README.md'),
-	long_description_content_type='text/markdown',
-	keywords='tasmania',
-	url='https://github.com/eth-cscs/tasmania',
-	license='',
-	package_dir={'': 'tasmania'},
-	packages=find_packages('tasmania'),
-	#package_data={'': ['tests/*', '*.pickle']},
-	setup_requires=['setuptools_scm', 'pytest-runner'],
-	tests_require=['pytest'],
-	install_requires=read_file('requirements.txt').split('\n'),
-	ext_package='grids.parser',
-	ext_modules=[Extension('parser_1d',
-						   sources=['tasmania/grids/parser/parser_1d_cpp.cpp',
-									'tasmania/grids/parser/parser_1d.cpp'],
-						   include_dirs=['tasmania/grids/parser']),
-				 Extension('parser_2d',
-						   sources=['tasmania/grids/parser/parser_2d_cpp.cpp',
-						   			'tasmania/grids/parser/parser_2d.cpp'],
-				 		   include_dirs=['tasmania/grids/parser'])],
-	classifiers=(
-		'Development Status :: 3 - Alpha',
-		'Programming Language :: Python :: 3',
-		'Operating System :: OS Independent',
-	),
-)
+    name='tasmania',
+    version='0.3.0',
+    author='Stefano Ubbiali',
+    author_email='subbiali@phys.ethz.ch',
+    description='A Python library to ease the composition, configuration, ' 
+                'and execution of Earth system models.',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='framework coupling',
+    url='https://github.com/eth-cscs/tasmania',
+    license='',
+    #package_dir={'': 'tasmania'},
+    packages=find_packages(),
+    install_requires=read_file('requirements.txt').split('\n'),
+    #package_data={'': ['tests/*', '*.pickle']},
+    setup_requires=['setuptools_scm', 'pytest-runner'],
+    tests_require=['pytest'],
+    ext_package='tasmania.cpp.parser',
+    ext_modules=[
+        Extension(
+            'parser_1d', 
+            sources=['tasmania/cpp/parser/parser_1d_cpp.cpp', 'tasmania/cpp/parser/parser_1d.cpp'],
+            include_dirs=['tasmania/cpp/parser']
+        ),
+        Extension(
+            'parser_2d',
+            sources=['tasmania/cpp/parser/parser_2d_cpp.cpp', 'tasmania/cpp/parser/parser_2d.cpp'],
+            include_dirs=['tasmania/cpp/parser']
+        )
+    ],
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
+)   
