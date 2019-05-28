3fa6cfc3308004c5ada0bdb4ca4d9d84a51b31ba
Stefano Ubbiali <subbiali@phys.ethz.ch>
2019-05-28 14:12:02 +0200

    Implemented print_time functionality to Plot and PlotComposite; added new figures.

--- tasmania/python/plot/animation.py
+++ tasmania/python/plot/animation.py
@@ -22,25 +22,11 @@
 #
 """
 This module contains:
-	get_time
 	Animation
 """
 import matplotlib.animation as manimation
 
 from tasmania.python.plot.monitors import PlotComposite
-
-
-def get_time(states):
-	for level0 in states:
-		if isinstance(level0, dict):  # level0 is a state dictionary
-			if 'time' in level0:
-				return level0['time']
-		else:  # level0 is a collection of state dictionaries
-			for level1 in level0:
-				if 'time' in level1:
-					return level1['time']
-
-	raise ValueError('No state dictionary contains the key ''time''.')
 
 
 class Animation:
@@ -49,32 +35,17 @@ class Animation:
 	:class:`~tasmania.Plot` or :class:`~tasmania.PlotComposite`
 	to generate the frames.
 	"""
-	def __init__(self, artist, print_time=None, init_time=None, fps=15):
+	def __init__(self, artist, fps=15):
 		"""
 		Parameters
 		----------
 		artist : `tasmania.Plot, tasmania.PlotComposite`
 			The *artist* actually generating the frames.
-		print_time : `str`, optional
-			String specifying if time should be printed above the plot,
-			flush with the right edge. Available options are:
-
-				* 'elapsed', to print the time elapsed since `init_time`;
-				* 'absolute', to print the absolute time of the snapshot;
-				* anything else, not to print anything.
-
-			Defaults to :obj:`None`.
-		init_time : `datetime`, optional
-			The initial time of the simulation. Only effective if `print_time`
-			is 'elapsed'. If not specified, the elapsed time is calculated
-			with respect to the first passed state.
 		fps : `int`, optional
 			Frames per second. Defaults to 15.
 		"""
 		# store input arguments as private attributes
 		self._artist = artist
-		self._ptime = print_time
-		self._itime = init_time
 		self._fps = fps
 
 		# ensure the artist is in non-interactive mode
@@ -128,34 +99,10 @@ class Animation:
 		# retrieve the figure object from the artist
 		fig = self._artist.figure
 
-		# save initial time
-		if self._ptime == 'elapsed' and self._itime is None:
-			self._itime = get_time(self._states[0])
-
 		with writer.saving(fig, save_dest, nt):
 			for n in range(nt):
 				# clean the canvas
 				fig.clear()
-
-				# get current time
-				if self._ptime in ('elapsed', 'absolute'):
-					time = get_time(self._states[n])
-
-				# get the string with the time
-				if self._ptime == 'elapsed':
-					time_str = str(time - self._itime)
-				elif self._ptime == 'absolute':
-					time_str = str(time)
-				else:
-					time_str = None
-
-				# update artist(s)'s properties
-				if time_str is not None:
-					if isinstance(self._artist, PlotComposite):
-						for subplot_artist in self._artist.artists:
-							subplot_artist.axes_properties['title_right'] = time_str
-					else:
-						self._artist.axes_properties['title_right'] = time_str
 
 				# create the frame
 				_ = self._artist.store(*self._states[n], fig=fig, show=False)
