0450de393682d59aa42773eaf67a06df03808b08
Stefano Ubbiali <subbiali@phys.ethz.ch>
2020-01-09 16:07:37 +0100

    Updated the scripts to build images and run containers on cpu.

--- docker/cpu/build_tasmania.sh
+++ docker/cpu/build_tasmania.sh
@@ -21,54 +21,87 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-CALL_DIR=$HOME/Desktop/phd/tasmania-develop-gt4py-v0.5.0/docker
-TASMANIA_ROOT=$HOME/Desktop/phd/tasmania-develop-gt4py-v0.5.0
-EXTERNAL_DIR=$CALL_DIR/external
-GT4PY_BRANCH=tasmania_migration
-DOCKERFILE=$CALL_DIR/cpu/dockerfiles/dockerfile.tasmania
+CDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TASMANIA_ROOT="$(dirname "$CDIR")"
+TASMANIA_ROOT="$(dirname "$TASMANIA_ROOT")"
+DOCKER_DIR=$TASMANIA_ROOT/docker
+EXTERNAL_DIR=$DOCKER_DIR/external
+GT4PY_BRANCH=master
+SYMPL_BRANCH=ubbiali
+DOCKERFILE=$CDIR/dockerfiles/dockerfile.tasmania
 IMAGE_NAME=tasmania:cpu
-IMAGE_SAVE=$CALL_DIR/images/tasmania_cpu.tar
+IMAGE_SAVE=$DOCKER_DIR/images/tasmania_cpu.tar
 
-echo "About to clone the gridtools4py repository under '$EXTERNAL_DIR/gridtools4py', and check out the '$GT4PY_BRANCH' branch."
+echo "About to clone the GridTools/gt4py repository under" 
+echo "'$EXTERNAL_DIR/gt4py',"
+echo "and check out the '$GT4PY_BRANCH' branch."
 read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
 echo ""
 
 if [[ $key = "" ]]; then
-	if [[ ! -d "$EXTERNAL_DIR/gridtools4py" ]]; then
-		git submodule add https://github.com/eth-cscs/gridtools4py.git $EXTERNAL_DIR/gridtools4py
+  echo ""
+
+	if [[ ! -d "$EXTERNAL_DIR/gt4py" ]]; then
+		git submodule add git@github.com:GridTools/gt4py.git "$EXTERNAL_DIR/gt4py"
 		git submodule update --init --recursive
 	fi
 
 	git submodule update --init --recursive
-	cd $EXTERNAL_DIR/gridtools4py
+	cd "$EXTERNAL_DIR/gt4py" || return
 	git checkout $GT4PY_BRANCH
-	cd $CALL_DIR
+	cd "$DOCKER_DIR" || return
 fi
 
 echo ""
-echo "About to copy a minimal version of '$TASMANIA_ROOT' under '$CALL_DIR/tasmania'."
+echo "About to clone the stubbiali/sympl repository under" 
+echo "'$EXTERNAL_DIR/sympl',"
+echo "and check out the '$SYMPL_BRANCH' branch."
 read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
 echo ""
 
 if [[ $key = "" ]]; then
-	if [[ ! -d "tasmania" ]]; then
-		mkdir tasmania
-	else
-		rm -rf tasmania/*
+  echo ""
+
+	if [[ ! -d "$EXTERNAL_DIR/sympl" ]]; then
+		git submodule add git@github.com:stubbiali/sympl.git "$EXTERNAL_DIR/sympl"
+		git submodule update --init --recursive
 	fi
 
-	cp -r $TASMANIA_ROOT/makefile tasmania
-	cp -r $TASMANIA_ROOT/notebooks tasmania
-	cp -r $TASMANIA_ROOT/README.md tasmania
-	cp -r $TASMANIA_ROOT/requirements.txt tasmania
-	cp -r $TASMANIA_ROOT/setup.cfg tasmania
-	cp -r $TASMANIA_ROOT/setup.py tasmania
-	cp -r $TASMANIA_ROOT/tasmania tasmania
-	cp -r $TASMANIA_ROOT/tests tasmania
+	git submodule update --init --recursive
+	cd "$EXTERNAL_DIR/sympl" || return
+	git checkout $SYMPL_BRANCH
+	cd "$DOCKER_DIR" || return
 fi
 
 echo ""
-echo "About to remove the tar archive '$IMAGE_SAVE' (if existing)."
+echo "About to copy a minimal version of"
+echo "'$TASMANIA_ROOT'" 
+echo "under '$TASMANIA_ROOT/tasmania'."
+read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
+echo ""
+
+if [[ $key = "" ]]; then
+  echo ""
+
+	if [[ ! -d "tasmania" ]]; then
+		mkdir $DOCKER_DIR/tasmania
+	else
+		rm -rf $DOCKER_DIR/tasmania/*
+	fi
+
+	cp -r $TASMANIA_ROOT/makefile $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/notebooks $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/README.md $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/requirements.txt $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/setup.cfg $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/setup.py $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/tasmania $DOCKER_DIR/tasmania
+	cp -r $TASMANIA_ROOT/tests $DOCKER_DIR/tasmania
+fi
+
+echo ""
+echo "About to remove the tar archive"
+echo "'$IMAGE_SAVE' (if existing)."
 read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
 echo ""
 
@@ -79,26 +112,35 @@ if [[ $key = "" ]]; then
 fi
 
 echo ""
-echo "About to build the image '$IMAGE_NAME' against the dockerfile '$DOCKERFILE'."
+echo "About to build the image '$IMAGE_NAME' against the dockerfile"
+echo "'$DOCKERFILE'."
 read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
 echo ""
 
 if [[ $key = "" ]]; then
-	cd $TASMANIA_ROOT && make distclean && cd $CALL_DIR
-	docker build --rm --build-arg uid=$(id -u) -f $DOCKERFILE -t $IMAGE_NAME .
+  echo ""
+	cd "$TASMANIA_ROOT" || return
+	make distclean
+	cd "$DOCKER_DIR" || return
+  docker build --rm \
+    --build-arg uid="$(id -u)" \
+    --build-arg gt4py_branch="$GT4PY_BRANCH" \
+    --build-arg sympl_branch="$SYMPL_BRANCH" \
+    -f "$DOCKERFILE" -t $IMAGE_NAME .
 fi
 
 echo ""
-echo "About to delete the folder '$CALL_DIR/tasmania'."
+echo "About to delete the folder '$DOCKER_DIR/tasmania'."
 read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
 echo ""
 
 if [[ $key = "" ]]; then
-	rm -rf tasmania
+	rm -rf "$DOCKER_DIR/tasmania"
 fi
 
 echo ""
-echo "About to save the image '$IMAGE_NAME' to the tar archive '$IMAGE_SAVE'."
+echo "About to save the image '$IMAGE_NAME' to the tar archive"
+echo "'$IMAGE_SAVE'."
 read -n 1 -s -r -p "Press ENTER to continue, CTRL-C to exit, or any other key to bypass this step." key
 echo ""
 
